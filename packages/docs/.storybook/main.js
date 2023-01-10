185fd5ad71fc4d1b158da59f128415e25e71eb0b
João Paulo Mesquita <jp-mesquita@live.com>
2023-01-10 17:59:11 -0300

    fix: add subpath  to Storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -15,5 +15,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, {configType}) => {
+    if(configType == 'PRODUCTION') {
+      config.base = '/Modulo-05_Design-System/'
+    }
+
+    return config
   }
 }
